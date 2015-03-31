279788c6e33ff82b1a98a005b884ccab0d385ef4
allensunbo <allensunbo@gmail.com>
2015-03-31 16:08:39 -0400

    $q

--- app/datepicker/app.js
+++ app/datepicker/app.js
@@ -4,18 +4,41 @@ angular.module('app', ['ui.bootstrap'])
       $scope.open = function () {
          console.log('open');
          $scope.mode = $scope.mode === 'hide' ? 'show' : 'hide';
-         $scope.dt[0] = new Date();
+         $scope.dt[0] = '2015-03-31';
 
       }
 
       $scope.mode = 'hide';
 
-      $scope.$watch(function () {
-         return $scope.dt[0];
-      }, function (newValue) {
-         console.log('changed:' + newValue);
+      $scope.$watchCollection(function () {
+         return $scope.dt;
+      }, function (newValue, oldValue) {
+         var index = arrayDiff(oldValue, newValue),
+            value = newValue[index];
+         if (!value) {
+            return;
+         }
+         for (var i = 0; i < 12; i++) {
+            $scope.dt[i] = undefined;
+         }
+         console.log(index);
+         console.log(value);
+         $scope.dt[index] = value;
       });
 
    });
+
+function arrayDiff(a, b) {
+   var len = Math.max(a.length, b.length);
+   for (var i = 0; i < len; i++) {
+      if (i < a.length && i < b.length) {
+         if (a[i] !== b[i]) return i;
+      } else if (i >= a.length && b[i] !== undefined) {
+         return i;
+      } else if (i >= b.length && a[i] !== undefined) {
+         return i;
+      }
+   }
+}
 
 
