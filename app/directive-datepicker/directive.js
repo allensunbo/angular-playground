dd2001736299204aca2ecbe0812e218aae6a7a10
allensunbo <allensunbo@gmail.com>
2015-03-31 20:15:22 -0400

    $q

--- app/directive-datepicker/directive.js
+++ app/directive-datepicker/directive.js
@@ -1,12 +1,16 @@
 angular.module('app')
    .directive('multipleDatepicker', function () {
       return {
-         restrict: "E",
+         restrict: 'E',
          templateUrl: 'multiple-datepicker.html',
-         link: function (scope) {
+         controller: 'DatepickerController',
+         link: function (scope, elem, attrs, datepickerController) {
+            console.log('linking....');
+            scope.dt = new Date();
             scope.open = function () {
                scope.mode = 'show';
             }
+            console.log(datepickerController.modes);
          }
       }
    });
